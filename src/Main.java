68ea9e6881653f75729a14bec7391d578bbeefd3
Nazarenko A A <boogerman1992@gmail.com>
2023-01-24 11:25:22 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int priceTiket = 299;
+        int bonusForMile = 20;
+
+        int bonus = priceTiket / bonusForMile;
+
+        System.out.println("Сумма бонусов равна " + bonus);
+    }
+}
